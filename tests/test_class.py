d8242a4ecf31d8b8c15edd70e34f1ae3fe6b6946
timelessnesses <timelessnesses@timelessnesses.me>
2024-03-02 02:41:04 +0700

    yes

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,77 +1,82 @@
 import unittest
-
-from timelessnesses.typed_env import TypedEnv, Method
 from enum import Enum
 from typing import Optional
+
+from timelessnesses.typed_env import Method, TypedEnv
+
 
 class WeirdEnum(Enum):
     a = 0
     b = 1
 
+
 class MyDotEnv(TypedEnv):
     sex: bool = False
+
 
 class MyDotEnv2(TypedEnv):
     sex: bool = False
     weird: WeirdEnum
 
+
 class TestClass(unittest.TestCase):
     def test_a(self):
         x = MyDotEnv()
-        x.get_env(Method.dotenv,dotenv="./tests/valid.env")
+        x.configure(Method.dotenv, dotenv="./tests/valid.env")
         x.load()
         assert x.sex
-    
+
     def test_b(self):
         x = MyDotEnv()
         with self.assertRaises(ValueError):
-            x.get_env(Method.env,dotenv="./tests/invalid.env")
+            x.configure(Method.env, dotenv="./tests/invalid.env")
             x.load()
-            
+
     def test_c(self):
         x = MyDotEnv()
         with self.assertRaises(ValueError):
-            x.get_env(Method.all)
+            x.configure(Method.all)
             x.load()
-            
+
     def test_d(self):
         x = MyDotEnv()
         with self.assertRaises(ValueError):
-            x.get_env(Method.dotenv,dotenv="./tests/invalid.env")
+            x.configure(Method.dotenv, dotenv="./tests/invalid.env")
             x.load()
-    
+
     def test_e(self):
         x = MyDotEnv()
-        x.get_env(Method.all, dotenv="./tests/valid.env")
-        x.raise_error_on_unknown_env(False)
+        x.configure(Method.all, dotenv="./tests/valid.env")
+        x.raise_error_on_unknown_env = False
         x.load()
         assert x.sex
-        
+
     def test_f(self):
         x = MyDotEnv()
-        x.get_env(Method.dotenv, dotenv="./tests/valid.env")
+        x.configure(Method.dotenv, dotenv="./tests/valid.env")
         x.load()
         assert x.export_as_dict() == {"sex": True}
 
     def test_g(self):
         x = MyDotEnv2()
-        x.get_env(Method.dotenv, dotenv="./tests/valid2.env")
+        x.configure(Method.dotenv, dotenv="./tests/valid2.env")
         x.add_validator(WeirdEnum, self.checker)
         x.load()
         assert x.weird == WeirdEnum.b
-        
+
     def checker(self, value: Optional[str]) -> WeirdEnum:
         if value is None:
             raise ValueError("Value is None")
-        
+
         return WeirdEnum(int(value))
-    
+
     def test_h(self):
         x = MyDotEnv2()
-        x.get_env(Method.dotenv, dotenv="./tests/invalid2.env")
+        x.configure(Method.dotenv, dotenv="./tests/invalid2.env")
         x.add_validator(WeirdEnum, self.checker)
         with self.assertRaises(ValueError):
             x.load()
+
 
 if __name__ == "__main__":
     unittest.main()
--- timelessnesses/typed_env.py
+++ timelessnesses/typed_env.py
@@ -1,10 +1,13 @@
 import datetime
+import os
+import typing
+import warnings
 from enum import Enum
 from json import loads
 from typing import Any, Callable, Optional
-import os
+
 from dotenv import dotenv_values
-import warnings
+
 
 def int_validator(value: Optional[str]) -> int:
     if value is None:
@@ -86,6 +89,7 @@ def bool_validator(value: Optional[str]) -> bool:
 def optional_bool_validator(value: Optional[str]) -> Optional[bool]:
     return None if value is None else bool_validator(value)
 
+
 def list_validator(value: Optional[str]) -> list:
     if value is None:
         raise ValueError("Value is None")
@@ -97,8 +101,10 @@ def list_validator(value: Optional[str]) -> list:
         except Exception as f:
             raise f from e
 
+
 def optional_list_validator(value: Optional[str]) -> Optional[list]:
     return None if value is None else list_validator(value)
+
 
 default_validators: dict[object, Callable[[Optional[str]], Any]] = {
     int: int_validator,
@@ -116,24 +122,30 @@ default_validators: dict[object, Callable[[Optional[str]], Any]] = {
     bool: bool_validator,
     Optional[bool]: optional_bool_validator,
     list: list_validator,
-    Optional[list]: optional_list_validator
+    Optional[list]: optional_list_validator,
 }
+
 
 class Method(Enum):
     dotenv = 0
     env = 1
     all = 2
 
+
+T = typing.TypeVar("T")
+
+
 class TypedEnv:
     """
     This class consists of methods to load environment variables from .env file.
     You should subclass this class then add your own types and default values.
-    Only supported types are\n
-    int\n
-    str\n
-    datetime.datetime \n
-    dict (JSON)\n
-    bool\n
+    Only supported types are
+    - `int`
+    - `str`
+    - `datetime.datetime`
+    - `dict` (JSON)
+    - `list`
+    - `bool`\n
     But you can extend it with your own type converters by using add_validator method with a type and a function that takes string (optionally).
     Or you can overwrite the validators dict to your own!
     """
@@ -141,7 +153,7 @@ class TypedEnv:
     types: dict[str, object] = {}
     validators: dict[object, Callable[[Optional[str]], object]] = {}
     envs: dict[str, Optional[str]] = {}
-    raise_error_on_unknown_env_op: bool = True
+    raise_error_on_unknown_env: bool = True
 
     def __init_subclass__(cls) -> None:
         cls.types = {}
@@ -150,35 +162,43 @@ class TypedEnv:
             cls.types[key] = value
         cls.validators.update(default_validators)
 
-    def get_env(self,method: Method=Method.dotenv,dotenv:Optional[str] = None,**kwargs):
+    def configure(
+        self, method: Method = Method.dotenv, dotenv: Optional[str] = None, **kwargs
+    ):
+        """Configure :class:`TypedEnv`
+
+        :param method: Method of loading the enviroment variable, defaults to Method.dotenv
+        :type method: Method, optional
+        :param dotenv: Dotenv file, defaults to None
+        :type dotenv: Optional[str], optional
+        :param kwargs: Additional arguments to pass to :py:func:dotenv.dotenv_values
+        :raises ValueError: dotenv argument is not needed when method is Method.env
+        :raises ValueError: dotenv argument is required when method is Method.dotenv or Method.all
+        :raises ValueError: Unknown Method
         """
-        This method loads environment variables from .env file or os.environ or both.
-        Default way to get enviroment variable is from .env file currently. You can change it with Method Enum.
-        You HAVE to call this method before load method.
-        """
-        
+
         if method == Method.env and dotenv is not None:
             raise ValueError("dotenv argument is not needed when method is Method.env")
         elif method in (Method.dotenv, Method.all) and dotenv is None:
-            raise ValueError("dotenv argument is required when method is Method.dotenv or Method.all")
+            raise ValueError(
+                "dotenv argument is required when method is Method.dotenv or Method.all"
+            )
         if method == Method.dotenv:
-            self.envs = dotenv_values(dotenv,**kwargs)
+            self.envs = dotenv_values(dotenv, **kwargs)
         elif method == Method.env:
             self.envs = dict(os.environ)
         elif method == Method.all:
-            self.envs = {**dict(os.environ),**dotenv_values(dotenv,**kwargs)}
+            self.envs = {**dict(os.environ), **dotenv_values(dotenv, **kwargs)}
         else:
             raise ValueError("Unknown method")
-        
-    def raise_error_on_unknown_env(self, enable: bool):
-        """
-        When this is enabled, if there is an environment variable that is not defined in the class, it will raise an error, else it will raise a warning.
-        """
-        self.raise_error_on_unknown_env_op = enable
 
     def load(self) -> None:
-        """
-        Load environment variables from .env file or os.environ or both based on get_env method.
+        """Load and store enviroment variable
+
+        :raises ValueError: No environment variables were loaded!
+        :raises ValueError: Error while parsing {key} (type {self.types[key]}).
+        :raises ValueError: Unknown type {self.types[key]}
+        :raises ValueError: Variable {key} was not set!
         """
         envs = self.envs
         if not envs:
@@ -189,7 +209,9 @@ class TypedEnv:
                 try:
                     value = self.validators[self.types[key]](value)
                 except ValueError as e:
-                    raise ValueError(f"Error while parsing {key} (type {self.types[key]}).") from e
+                    raise ValueError(
+                        f"Error while parsing {key} (type {self.types[key]})."
+                    ) from e
                 except KeyError as e:
                     raise ValueError(f"Unknown type {self.types[key]}") from e
                 if value is not None:
@@ -201,7 +223,7 @@ class TypedEnv:
                     elif not hasattr(self, key) and self.types[key] is Optional:
                         setattr(self, key, None)
                 continue
-            if self.raise_error_on_unknown_env_op:
+            if self.raise_error_on_unknown_env:
                 raise ValueError(f"Unknown variable {key}")
             else:
                 warnings.warn(f"Unknown variable {key}")
@@ -216,20 +238,20 @@ class TypedEnv:
                     else:
                         setattr(self, key, None)
 
-    def add_validator(
-        self, type_: object, validator: Callable[[Optional[str]], object]
-    ) -> None:
-        """
-        Add a validator for a type.
-        This is important if you have custom type like a class or an enum.
-        NOTE: If there's same type it will get overwritten.
+    def add_validator(self, type_: T, validator: Callable[[Optional[str]], T]) -> None:
+        """Adds a new validator for type checking
+
+        :param type_: New type
+        :type type_: T
+        :param validator: A function that accepts `str` or `None` and returns `T`
+        :type validator: Callable[[Optional[str]], T]
         """
         self.validators[type_] = validator
-    
+
     def export_as_dict(self) -> dict:
-        
-        """
-        Exporting all variables as a dict.
-        NOTE: This will not export variables that are not set. So you had to call load method before this.
+        """Exporting entire thing as dictionary
+
+        :return: Entire enviroment variable as a dictionary
+        :rtype: dict
         """
         return {key: getattr(self, key) for key in self.types.keys()}
